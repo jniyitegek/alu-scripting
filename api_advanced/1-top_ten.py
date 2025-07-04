67be2dc6a29cc3f32db8f403709c3d5f47a491c0
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-07-04 16:30:27 +0200

    the advanced api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
