0acd016dfd2efdeebe06d682cf958b3b460f19f2
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-07-04 17:10:58 +0200

    the advanced api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""
+Script that fetches the 10 hot posts for a given subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints 'None'.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
         "User-Agent": "MyRedditApp/1.0"
     }
     params = {"limit": 10}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
