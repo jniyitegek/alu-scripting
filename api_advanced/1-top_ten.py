06fc8d76ee7633d80b6f2a3d9be5ebef3e348323
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-07-04 17:00:07 +0200

    the advanced api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """Script that fetch 10 hot post for a given subreddit."""
-
-
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.if not return 0."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        "User-Agent": "MyRedditApp/1.0"
-    }
-    params = {"limit": 10}
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
-        return
-
-    data = response.json()
-    posts = data.get("data", {}).get("children", [])
-    if not posts:
-        print(None)
-        return
-
-    for post in posts:
-        print(post["data"]["title"])
